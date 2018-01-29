027d7bda11d238f62fdbe6fc09af085365cc6f94
bkeith <bnkeith.20@gmail.com>
2018-01-29 09:44:18 -0500

    First Commit

---
+++ Tests/edu/ithaca/bkeith/simpleStringCalculatorTest.java
@@ -0,0 +1,18 @@
+package edu.ithaca.bkeith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class simpleStringCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void AddTest() {
+        //empty string (should return 0)
+
+        //one number: positive, negative, numbers of more than one digit
+
+        //two numbers: positive, negative, numbers of more than one digit
+
+        //more than two numbers
+
+    }
+}
---
+++ src/edu/ithaca/bkeith/simpleStringCalculator.java
@@ -0,0 +1,10 @@
+package edu.ithaca.bkeith;
+
+public class simpleStringCalculator {
+
+    int Add(String numbers){
+        int sum = 0;
+        return sum;
+    }
+
+}
