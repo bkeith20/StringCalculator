ea7a5301bf3c24971af2b1b61e1718f2f1483498
bkeith <bnkeith.20@gmail.com>
2018-01-31 14:09:49 -0500

    Two integer test

--- Tests/edu/ithaca/bkeith/simpleStringCalculatorTest.java
+++ Tests/edu/ithaca/bkeith/simpleStringCalculatorTest.java
@@ -11,6 +11,7 @@ class simpleStringCalculatorTest {
         String nums = "";
         int sum = myCalc.Add(nums);
         assertEquals(0,sum);
+
         //one number: positive, negative, numbers of more than one digit
         nums = "2";
         sum = myCalc.Add(nums);
@@ -25,6 +26,17 @@ class simpleStringCalculatorTest {
         assertEquals(100,sum);
 
         //two numbers: positive, negative, numbers of more than one digit
+        nums = "2,3";
+        sum = myCalc.Add(nums);
+        assertEquals(5,sum);
+
+        nums = "-2,-4";
+        sum = myCalc.Add(nums);
+        assertEquals(-6,sum);
+
+        nums = "100,-1";
+        sum = myCalc.Add(nums);
+        assertEquals(99,sum);
 
         //more than two numbers
 
